1459aa170beb3e5269cc72d442c621336158224e
cools7 <15003862279@139.com>
2018-06-13 15:35:23 +0800

    Site updated: 2018-06-13 15:35:19

--- js/src/utils.js
+++ js/src/utils.js
@@ -10,7 +10,7 @@ NexT.utils = NexT.$u = {
       .not('.group-picture img, .post-gallery img')
       .each(function () {
         var $image = $(this);
-        // if ($(this).hasClass('nofancybox')) return;
+        if ($(this).hasClass('nofancybox')) return;
         var imageTitle = $image.attr('title');
         var $imageWrapLink = $image.parent('a');
 
@@ -30,13 +30,13 @@ NexT.utils = NexT.$u = {
         }
       });
 
-    $('.fancybox').fancybox({
-      helpers: {
-        overlay: {
-          locked: false
-        }
-      }
-    });
+    // $('.fancybox').fancybox({
+    //   helpers: {
+    //     overlay: {
+    //       locked: false
+    //     }
+    //   }
+    // });
   },
 
   lazyLoadPostsImages: function () {
